cc1f1b3e0e61376c4a8a138f9aae46506eb0ec38
gabrieltele <141058254+gabrieltele@users.noreply.github.com>
2024-08-20 17:40:14 -0300

    home
    mudei o home todo

---
+++ home/script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+mostrarSlides(slideIndex);
+
+// Função para mudar de imagem
+function mudarImagem(n) {
+    mostrarSlides(slideIndex += n);
+}
+
+function mostrarSlides(n) {
+    let slides = document.querySelectorAll('.carousel-slide img');
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    let offset = -slideIndex * 20;
+    document.querySelector('.carousel-slide').style.transform = `translateX(${offset}%)`;
+}
+
+// Carrossel automático a cada 5 segundos
+setInterval(() => {
+    mudarImagem(1);
+}, 2000);
