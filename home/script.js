06d19f49b8b7ac5d6916bbab86cea993d1fa6492
gabrieltele <141058254+gabrieltele@users.noreply.github.com>
2024-08-26 14:15:14 -0300

    mudei o home
    coloque img nas noticias

--- home/script.js
+++ home/script.js
@@ -6,19 +6,22 @@ function mudarImagem(n) {
     mostrarSlides(slideIndex += n);
 }
 
+// Função para mostrar slides
 function mostrarSlides(n) {
     let slides = document.querySelectorAll('.carousel-slide img');
     if (n >= slides.length) {
-        slideIndex = 0;
+        slideIndex = 0; // Volta ao primeiro slide
+    } else if (n < 0) {
+        slideIndex = slides.length - 1; // Vai para o último slide
+    } else {
+        slideIndex = n; // Atualiza o índice do slide
     }
-    if (n < 0) {
-        slideIndex = slides.length - 1;
-    }
-    let offset = -slideIndex * 25;
+    
+    let offset = -slideIndex * 25; // Ajuste o offset para 100%
     document.querySelector('.carousel-slide').style.transform = `translateX(${offset}%)`;
 }
 
-// Carrossel automático a cada 5 segundos
+// Carrossel automático a cada 4 segundos
 setInterval(() => {
     mudarImagem(1);
 }, 4000);
