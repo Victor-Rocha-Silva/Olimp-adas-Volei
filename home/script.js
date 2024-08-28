4a6948f499fd6cb91181a9a8f93ebf23c6cab21d
gabrieltele <141058254+gabrieltele@users.noreply.github.com>
2024-08-28 14:46:36 -0300

    asdaas

--- home/script.js
+++ home/script.js
@@ -1,4 +1,5 @@
 let slideIndex = 0;
+
 mostrarSlides(slideIndex);
 
 // Função para mudar de imagem
@@ -9,19 +10,18 @@ function mudarImagem(n) {
 // Função para mostrar slides
 function mostrarSlides(n) {
     let slides = document.querySelectorAll('.carousel-slide img');
+    let carouselWidth = document.querySelector('.carousel-slide').offsetWidth; // Obtenha a largura total do carrossel
     if (n >= slides.length) {
-        slideIndex = 0; // Volta ao primeiro slide
-    } else if (n < 0) {
-        slideIndex = slides.length - 1; // Vai para o último slide
+        slideIndex = 0; // Reset to the first slide
     } else {
         slideIndex = n; // Atualiza o índice do slide
     }
     
-    let offset = -slideIndex * 25; // Ajuste o offset para 100%
-    document.querySelector('.carousel-slide').style.transform = `translateX(${offset}%)`;
+    let offset = -slideIndex * (carouselWidth / slides.length); // Calcula o offset correto
+    document.querySelector('.carousel-slide').style.transform = `translateX(${offset}px)`;
 }
 
 // Carrossel automático a cada 4 segundos
 setInterval(() => {
     mudarImagem(1);
-}, 4000);
+}, 10000);
